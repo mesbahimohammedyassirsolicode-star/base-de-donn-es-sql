d1a9e090e5ade56d5b41dd24cd06f25671718ed1
https://github.com/mesbahimohammedyassirsolicode-star <mesbahi.mohammedyassir.solicode@gmail.com>
2025-12-26 09:25:41 +0100

    updt

--- Réalisation/réalisation.sql
+++ Réalisation/réalisation.sql
@@ -1,45 +1,47 @@
--- Création de la base de données
-CREATE DATABASE blog_db;
+CREATE DATABASE IF NOT EXISTS blog_db;
 USE blog_db;
-
--- Table UTILISATEUR
-CREATE TABLE utilisateur (
+ -- table utilisateur
+ CREATE TABLE utilisateur(
     id_utilisateur INT AUTO_INCREMENT PRIMARY KEY,
-    nom VARCHAR(100) NOT NULL,
-    email VARCHAR(150) UNIQUE NOT NULL,
-    mot_de_passe VARCHAR(255) NOT NULL,
-    date_inscription DATE DEFAULT CURRENT_DATE
-);
+	nom VARCHAR(100) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+	mot_de_passe VARCHAR(255)  NOT NULL,
+	date_inscreption DATETIME DEFAULT CURRENT_TIMESTAMP,
+    ALTER TABLE Utilisateur COMMENT = 'Table des utilisateurs du blog';
 
--- Table CATEGORIE
-CREATE TABLE categorie (
-    id_categorie INT AUTO_INCREMENT PRIMARY KEY,
-    nom_categorie VARCHAR(100) NOT NULL,
-    description TEXT
-);
+ )	;
+ -- table CATEGORY
+  CREATE TABLE category(
+    id_category INT AUTO_INCREMENT PRIMARY KEY,
+	nom VARCHAR(100) NOT NULL UNIQUE,
+    ALTER TABLE category COMMENT = 'Table des categories du blog';
 
--- Table ARTICLE
-CREATE TABLE article (
+ )	;
+ -- table ARTICLE
+ CREATE TABLE article(
     id_article INT AUTO_INCREMENT PRIMARY KEY,
-    titre VARCHAR(200) NOT NULL,
-    contenu TEXT NOT NULL,
-    date_publication DATE,
-    statut ENUM('publie', 'brouillon') DEFAULT 'brouillon',
-    id_utilisateur INT NOT NULL,
-    id_categorie INT NOT NULL,
-    FOREIGN KEY (id_utilisateur) REFERENCES utilisateur(id_utilisateur),
-    FOREIGN KEY (id_categorie) REFERENCES categorie(id_categorie)
-);
+	titre VARCHAR(100) NOT NULL ,
+    contenue TEXT NOT NULL,
+	mot_de_passe VARCHAR(255)  NOT NULL,
+	date_publication DATETIME DEFAULT CURRENT_TIMESTAMP,
+    statut ENUM('publie','brouillon') NOT NULL,
+    id_utilisateur INT,
+	id_category INT,
+     FOREIGN KEY (id_utilisateur) REFERENCES utilisateur(id_utilisateur),
+     FOREIGN KEY (id_category) REFERENCES category(id_category),
+     ALTER TABLE article COMMENT = 'Table des articles du blog';
 
--- Table COMMENTAIRE
-CREATE TABLE commentaire (
+     
+ )	;
+ -- table COMMENTAIRE
+ CREATE TABLE commentaire(
     id_commentaire INT AUTO_INCREMENT PRIMARY KEY,
-    contenu TEXT NOT NULL,
-    date_publication DATE DEFAULT CURRENT_DATE,
-    id_utilisateur INT NOT NULL,
-    id_article INT NOT NULL,
-    FOREIGN KEY (id_utilisateur) REFERENCES utilisateur(id_utilisateur),
-    FOREIGN KEY (id_article) REFERENCES article(id_article)
-);
+	date_commentaire DATETIME DEFAULT CURRENT_TIMESTAMP,
+    id_utilisateur INT,
+	id_article INT,
+     FOREIGN KEY (id_utilisateur) REFERENCES utilisateur(id_utilisateur),
+     FOREIGN KEY (id_article) REFERENCES article(id_article),
+     ALTER TABLE commentaire COMMENT = 'Table des commentaires du blog';
 
-select id_article from article
+     
+ )	;
